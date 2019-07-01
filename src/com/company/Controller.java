d6fa46b66b0741b887e9a23bb1b917043603eb38
samballa07 <seth.amballa@gmail.com>
2019-07-01 09:33:45 -0400

    Ver 2.5

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -74,7 +74,7 @@ public class Controller {
             System.out.println("\t\t\t\tSELECT SHOE(S)");
             ShoesSelect(closetIndexToPack);
         }
-        System.out.println(ANSI_BLUE + "\t\t\t\tYour suit case has: " + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "\t\t\t\tYOUR SUIT CASE HAS: " + ANSI_RESET);
         this.suitCase.forEach(System.out::println);
     }
 
@@ -167,7 +167,9 @@ public class Controller {
     private boolean isOneShoeInSuitCase(int closetIndex) {
         String userAnswer;
         boolean isOneShoeInList = false;
-        for (int i = 0; i < this.closetList.get(closetIndex).getShoesList().size(); i++) {
+        int flipFlopIndex;
+        int i = 0;
+        for (; i < this.closetList.get(closetIndex).getShoesList().size(); i++) {
             System.out.println("Do you want to add this "
                     + this.closetList.get(closetIndex).getShoesList().get(i)
                     + " to your suit case? Press yes to add or any key to refuse.");
@@ -176,23 +178,31 @@ public class Controller {
                 this.suitCase.add(this.closetList.get(closetIndex).getShoesList().get(i).toString());
                 this.trackingShoes.add(this.closetList.get(closetIndex).getShoesList().get(i));
                 isOneShoeInList = true;
+
                 if (this.trackingShoes.size() == 1 &&
                         this.closetList.get(closetIndex).getShoesList().get(i).getKindOfShoes().equals("Flip Flop")) {
                     this.isFlipFlop = true;
+
+
                 }
             }
         }
+
+        for (int j = 0; j < this.trackingShoes.size(); j++) {
+            this.closetList.get(closetIndex).getShoesList().remove(this.trackingShoes.get(j));
+        }
+
         while (this.isFlipFlop) {
             System.out.println("\t\t\t\tYou should pack OTHER PAIR(S) OF SHOES");
-            for (int i = 0; i < this.closetList.get(closetIndex).getShoesList().size(); i++) {
+            for (int j = 0; j < this.closetList.get(closetIndex).getShoesList().size(); j++) {
                 System.out.println("Do you want to add this "
-                        + this.closetList.get(closetIndex).getShoesList().get(i)
+                        + this.closetList.get(closetIndex).getShoesList().get(j)
                         + " to your suit case? Press yes to add or any key to refuse.");
                 userAnswer = new Scanner(System.in).nextLine();
                 if (userAnswer.equalsIgnoreCase("Yes")) {
-                    this.suitCase.add(this.closetList.get(closetIndex).getShoesList().get(i).toString());
+                    this.suitCase.add(this.closetList.get(closetIndex).getShoesList().get(j).toString());
                     isOneShoeInList = true;
-                    if (!this.closetList.get(closetIndex).getShoesList().get(i).getKindOfShoes().equals("Flip Flop")) {
+                    if (!this.closetList.get(closetIndex).getShoesList().get(j).getKindOfShoes().equals("Flip Flop")) {
                         this.isFlipFlop = false;
                     }
                 }
@@ -200,209 +210,6 @@ public class Controller {
         }
         return isOneShoeInList;
     }
-
-
-//    public void JacketSelect(int closetIndex) {
-//        int countJacket = 0;
-//        boolean isInList = true;
-//        String userAnswer;
-//
-//        for (int i = 0; i < this.closetList.get(closetIndex).getJacketList().size(); i++) {
-//            System.out.println("Do you want to add this in your suit case?");
-//            userAnswer = new Scanner(System.in).nextLine();
-//            if (userAnswer.equalsIgnoreCase("Yes")) {
-//                this.suitCase.add(this.closetList.get(closetIndex).getJacketList().get(i).toString());
-//            }
-//        }
-//    }
-//
-//    public void JacketSelect(int closetIndex) {
-//        int countJacket = 0;
-//        boolean isInList = true;
-//        String userAnswer;
-//
-//        for (int i = 0; i < this.closetList.get(closetIndex).getJacketList().size(); i++) {
-//            System.out.println("Do you want to add this in your suit case?");
-//            userAnswer = new Scanner(System.in).nextLine();
-//            if (userAnswer.equalsIgnoreCase("Yes")) {
-//                this.suitCase.add(this.closetList.get(closetIndex).getJacketList().get(i).toString());
-//            }
-//        }
-//    }
-
-    //
-//    private final int THREE = 3;
-//    private String closetName;
-//    private List<Jacket> jacketList = new ArrayList<>();
-//    private List<Shirt> shirtList = new ArrayList<>();
-//    private List<Pants> pantsList = new ArrayList<>();
-//    private List<Shoes> shoesList = new ArrayList<>();
-//    private List<Closet> closetsList = new ArrayList<>();
-//
-//    public Controller() {
-//        this.jacketList.add(new Jacket("cotton", "blue"));
-//        this.jacketList.add(new Jacket("linen", "Opal"));
-//        this.jacketList.add(new Jacket("feather", "red"));
-//        this.shirtList.add(new Shirt("linen", "black"));
-//        this.shirtList.add(new Shirt("feather", "red"));
-//        this.shirtList.add(new Shirt("cotton", "blue"));
-//        this.pantsList.add(new Pants("feather", "red"));
-//        this.pantsList.add(new Pants("linen", "black"));
-//        this.pantsList.add(new Pants("cotton", "blue"));
-//        this.shoesList.add(new Shoes("snicker", "feather", "green"));
-//        this.shoesList.add(new Shoes("flip flop", "feather", "red"));
-//        this.shoesList.add(new Shoes("sandal", "feather", "black"));
-//    }
-//
-//    public void GenerateCloset() {
-//        int randomNum = 0;
-//        String closetName;
-//
-//        System.out.println("Input three closets name");
-//        for (int i = 0; i < THREE; i++) {
-//            randomNum = new Random().nextInt(THREE);
-//            closetName = new Scanner(System.in).nextLine();
-//
-//            this.closetsList.add(new Closet(closetName, this.jacketList.get(randomNum), this.shirtList.get(randomNum), this.pantsList.get(randomNum), this.shoesList.get(randomNum)));
-////            this.closetsList.add(closetName, this.jacketList.get(i), this.shirtList.get(i), this.pantsList.get(i), this.shoesList.get(i));
-//        }
-//
-//        System.out.println("Here are three suggestion for you to pack: ");
-//        for (int i = 0; i < this.closetsList.size(); i++) {
-//            System.out.println(this.closetsList.get(i).toString());
-//        }
-//    }
-//
-//    public void SelectSuitCase() {
-//        String userInput;
-//        String userInput2;
-//        int randomNum = 0;
-//        System.out.println("Enter the closet you want to pack: ");
-//        userInput = new Scanner(System.in).nextLine();
-//        for (int i = 0; i < this.closetsList.size(); i++) {
-//            if (this.closetsList.get(i).getName().equalsIgnoreCase(userInput)) {
-//                if (this.closetsList.get(i).getShoes().getKindOfShoes().equalsIgnoreCase("flip flop")) {
-//                    System.out.println("Do you want to add other kind of shoes?");
-//                    userInput2 = new Scanner(System.in).nextLine();
-//                    if (userInput2.equalsIgnoreCase("yes")) {
-//                        randomNum = new Random().nextInt(THREE);
-//                        while (this.shoesList.get(randomNum).getKindOfShoes().equalsIgnoreCase("flip flop")) {
-//                            randomNum = new Random().nextInt(THREE);
-//                        }
-//                        this.closetsList.add(new Closet(this.closetsList.get(i).getName(), this.jacketList.get(randomNum), this.shirtList.get(randomNum), this.pantsList.get(randomNum), this.shoesList.get(randomNum)));
-//                    }
-//                    if (!userInput2.equalsIgnoreCase("yes")){
-//                        System.out.println("Program End");
-//                    }
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    public void DisplaySuitCaseByName() {
-//        String userInput;
-//        System.out.println("Enter the suitcase you want to look for: ");
-//        userInput = new Scanner(System.in).nextLine();
-//        for (int i = 0; i < this.closetsList.size(); i++) {
-//            if (this.closetsList.get(i).getName().equalsIgnoreCase(userInput)) {
-//                System.out.println(this.closetsList.get(i).toString());
-//            }
-//        }
-//    }
-
-//    public void DisplayShoesByName(){
-//        String userInput;
-//        System.out.println("Enter the suitcase you want to look for: ");
-//        userInput = new Scanner(System.in).nextLine();
-//        for (int i = 0; i < this.closetsList.size(); i++) {
-//            if (this.closetsList.get(i).getName().equalsIgnoreCase(userInput)) {
-//                System.out.println(this.closetsList.get(i).toString());
-//            }
-//        }
-//    }
-
-
-//    private List<Jacket> AddingJackets() {
-//        int randomNum = 0;
-//        for (int i = 0; i < THREE; i++) {
-//            randomNum = new Random().nextInt(THREE);
-//        }
-//        switch (randomNum) {
-//            case 0:
-//                this.jacketList.add(new Jacket("cotton", "blue"));
-//                break;
-//            case 1:
-//                this.jacketList.add(new Jacket("linen", "Opal"));
-//                break;
-//            case 2:
-//                this.jacketList.add(new Jacket("feather", "red"));
-//                break;
-//        }
-////        }
-//        return jacketList;
-//    }
-
-//    private List<Shirt> AddingShirts() {
-//        int randomNum = 0;
-//        for (int i = 0; i < THREE; i++) {
-//            randomNum = new Random().nextInt(THREE);
-//        }
-//        switch (randomNum) {
-//            case 0:
-//                this.shirtList.add(new Shirt("linen", "black"));
-//
-//            case 1:
-//                this.shirtList.add(new Shirt("feather", "red"));
-//
-//            case 2:
-//                this.shirtList.add(new Shirt("cotton", "blue"));
-//                break;
-//        }
-//        return shirtList;
-//    }
-//        return null;
-
-
-//    private List<Pants> AddingPants() {
-//        int randomNum = 0;
-//        for (int i = 0; i < THREE; i++) {
-//            randomNum = new Random().nextInt(THREE);
-//        }
-//        switch (randomNum) {
-//            case 0:
-//                this.pantsList.add(new Pants("feather", "red"));
-//                break;
-//            case 1:
-//                this.pantsList.add(new Pants("linen", "black"));
-//                break;
-//            case 2:
-//                this.pantsList.add(new Pants("cotton", "blue"));
-//                break;
-//        }
-////        }
-//        return pantsList;
-//    }
-
-//    private List<Shoes> AddingShoes() {
-//        int randomNum = 0;
-//        for (int i = 0; i < THREE; i++) {
-//            randomNum = new Random().nextInt(THREE);
-//        }
-//        switch (randomNum) {
-//            case 0:
-//                this.shoesList.add(new Shoes("snicker", "feather", "green"));
-//                break;
-//            case 1:
-//                this.shoesList.add(new Shoes("flip flop", "feather", "red"));
-//                break;
-//            case 2:
-//                this.shoesList.add(new Shoes("sandal", "feather", "black"));
-//                break;
-//        }
-////        }
-//        return shoesList;
-//    }
 
 
 }
